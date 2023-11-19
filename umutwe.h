73f701c724b6ae1f4af926b844b7b780a72b06dc
Ndiramiye Ninshuti Poli <113962333+Mr-Ndi@users.noreply.github.com>
2023-11-19 11:41:51 +0200

    Add files via upload

---
+++ umutwe.h
@@ -0,0 +1,16 @@
+#ifndef UMUTWE_H
+#define UMUTWE_H
+
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+
+int generete(int level);
+int Asking();
+void playing();
+void Rounds(int inshuro);
+int Intro();
+
+#endif
